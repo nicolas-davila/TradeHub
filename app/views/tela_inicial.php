9f3e5fd7859b87c302c8aed9a7ef37a212f62a47
nicolas-davila <nicolasdavilafsantos@gmail.com>
2025-09-24 20:08:39 -0300

    Adicionando arquivos teste

---
+++ app/views/tela_inicial.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Produtos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="container mt-5">
+<h1 class="mb-4">Lista de Produtos</h1>
+<table class="table table-bordered">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Produto</th>
+        <th>Preço (R$)</th>
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Teste</td>
+            <td>Produto 1</td>
+            <td>Produto 2</td>
+        </tr>
+    </tbody>
+</table>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+// Inicia a página
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Produtos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="container mt-5">
+
+    <?php
+        // Inclui o HTML da view
+        include __DIR__ . "/../app/views/tela_inicial.php";
+    ?>
+
+</body>
+</html>
